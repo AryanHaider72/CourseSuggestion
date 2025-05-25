7647c935eab64576a80e2c5ef488604e36317202
AryanHaider72 <aryanhaider408@gmail.com>
2025-05-25 10:57:31 +0500

    Initial commit

--- app/AdminComponent/BillingPayment/page.js
+++ app/AdminComponent/BillingPayment/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Table, Button, Card, Form, InputGroup, Badge, Spinner } from 'react-bootstrap';
--- app/component/payments/page.js
+++ app/component/payments/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState,useRef } from "react";
 import CourseSuggestions from "../suggestion/page";
 import { ChevronUp } from "lucide-react";
